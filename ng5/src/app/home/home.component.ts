97418efd3839288ebc496254cd9bc91b313c2e96
Raja Pasupuleti <raja.pasupuleti@gatech.edu>
2018-05-27 23:12:02 -0400

    bucket list as home

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  //templateUrl: './home.component.html',
-  template:`<h1> THIS IS FROM INLINE TEMPLATE FROM HOME`,
+  templateUrl: './home.component.html',
+  //template:`<h1> THIS IS FROM INLINE TEMPLATE FROM HOME`,
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
