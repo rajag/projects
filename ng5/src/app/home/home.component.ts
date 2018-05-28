aec6253f1b3c17d86b372529f45a3e283c7544b6
Raja Pasupuleti <raja.pasupuleti@gatech.edu>
2018-05-27 23:10:00 -0400

    inline template

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  //templateUrl: './home.component.html',
+  template:`<h1> THIS IS FROM INLINE TEMPLATE FROM HOME`,
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
