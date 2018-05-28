341a053e357788c3b9562b45fb7b6a141c5b5cf0
Raja Pasupuleti <raja.pasupuleti@gatech.edu>
2018-05-27 23:19:52 -0400

    Added global sytles

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -4,12 +4,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-home',
   templateUrl: './home.component.html',
   // template:`<h1> THIS IS FROM INLINE TEMPLATE FROM HOME`,
-// styleUrls: ['./home.component.scss']
+ styleUrls: ['./home.component.scss']
+/*
 // inline  styles
 styles[ `
   p { font-weight: bold; }
   div { color: gray; }
   `]
+  */
 })
 export class HomeComponent implements OnInit {
 
