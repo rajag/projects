946ed8a51d858722445549b81a83e3a8c62ef339
Raja Pasupuleti <raja.pasupuleti@gatech.edu>
2018-05-27 23:33:23 -0400

    interpolation of properties

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -4,16 +4,18 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-home',
   templateUrl: './home.component.html',
   // template:`<h1> THIS IS FROM INLINE TEMPLATE FROM HOME`,
- styleUrls: ['./home.component.scss']
-/*
-// inline  styles
-styles[ `
-  p { font-weight: bold; }
-  div { color: gray; }
-  `]
-  */
+  styleUrls: ['./home.component.scss']
+  /*
+  // inline  styles
+  styles[ `
+    p { font-weight: bold; }
+    div { color: gray; }
+    `]
+    */
 })
 export class HomeComponent implements OnInit {
+
+  itemCount: number = 4;
 
   constructor() { }
 
