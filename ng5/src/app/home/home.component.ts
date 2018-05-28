331b8e976a40567f7afd2543855cc3ea98381d5e
Raja Pasupuleti <raja.pasupuleti@gatech.edu>
2018-05-27 23:16:31 -0400

    inline styles

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -3,8 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  //template:`<h1> THIS IS FROM INLINE TEMPLATE FROM HOME`,
-  styleUrls: ['./home.component.scss']
+  // template:`<h1> THIS IS FROM INLINE TEMPLATE FROM HOME`,
+// styleUrls: ['./home.component.scss']
+// inline  styles
+styles[ `
+  p { font-weight: bold; }
+  div { color: gray; }
+  `]
 })
 export class HomeComponent implements OnInit {
 
